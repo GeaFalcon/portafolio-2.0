0ce39e6d57a90c823ccb7e562c8dec920592075c
Sknor <geafalconjoseantonio@gmail.com>
2021-02-25 00:10:21 +0100

    Cv

--- portafolio/src/componentes/Curriculum/Curriculum.js
+++ portafolio/src/componentes/Curriculum/Curriculum.js
@@ -1,23 +1,33 @@
 import React from "react";
 import "./Curriculum.scss";
 import icono from "../media/man.svg";
+import github from "../media/signo-de-github.svg";
+import linkedin from "../media/linkedin.svg";
 
 const Curriculum = () => {
   return (
-    <div>
+    <div className="fondo-cv ">
       <div className="cv">
-        <span>{icono}</span>
-        <p>TRABAJEMOS JUNTOS</p>
-        <hr />
+        <img className="reverse" src={icono} />
+        <h2>
+          TRABAJEMOS JUNTOS
+          <hr />
+        </h2>
         <p>
           Quiero seguir aprendiendo y creciendo profesionalmente. Si estás
           interesado/a en un perfil de desarrolladora front-end junior creativa
           y con muchas ganas de aprender, contacta conmigo.
         </p>
-        <button></button>
+        <h3 className="button">Descargar CV</h3>
+
+        <div className="contacto">
+          <span>Diseño: Irene Rueda | Javier Santos</span>
+          <div className="icon-contacto">
+            <img className="icon" src={linkedin} />
+            <img className="reverse icon" src={github} />
+          </div>
+        </div>
       </div>
-      <div>{icono}</div>
-      <div className="vacio"></div>
     </div>
   );
 };
